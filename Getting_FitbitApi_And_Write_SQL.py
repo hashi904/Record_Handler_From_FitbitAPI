8c8afdb1178f0c2d2189754a9b4952da8eec5f39
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2020-01-06 18:11:22 +0900

    firtst_commit

---
+++ Getting_FitbitApi_And_Write_SQL.py
@@ -0,0 +1,72 @@
+#必要なライブラリーをインポート
+import fitbit
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+%matplotlib inline
+import os
+from datetime import date, timedelta
+import datetime
+import psycopg2
+
+#fitbit apiを作動させるのに必要な環境変数をosから取得
+CLIENT_ID = os.environ['FITBIT_CLIENT_ID']
+CLIENT_SECRET = os.environ['FITBIT_CLIENT_SECRET']
+ACCESS_TOKEN = os.environ['FITBIT_ACCESS_TOKEN']
+REFRESH_TOKEN = os.environ['FITBIT_REFRESH_TOKEN']
+
+#accesstokenの有効期限が切れていても自動取得させる関数定義
+def refresh_cb(token_dict):
+    global ACCESS_TOKEN
+    global REFRESH_TOKEN
+    ACCESS_TOKEN = token_dict['access_token']
+    REFRESH_TOKEN = token_dict['refresh_token']
+    return token_dict
+
+#refresh_cb=refresh_cbでaccess tokenを自動更新する
+authd_client = fitbit.Fitbit(
+    CLIENT_ID,
+    CLIENT_SECRET,
+    access_token=ACCESS_TOKEN,
+    refresh_token=REFRESH_TOKEN,
+    refresh_cb=refresh_cb
+)
+
+#DBに接続
+conn = psycopg2.connect("host=127.0.0.1 port=5432 dbname=DataRecordApp-db user=root password=")
+cur = conn.cursor()
+
+#1日分 リクエスト回数24回/日
+#================
+#1月から取得する
+#================
+for DAY in range(28, 32):
+    if DAY < 10 :
+        DATE = "2019-12-0" + str(DAY)
+              
+    else :
+        DATE = "2019-12-" + str(DAY)
+        
+    #stringdateカラムにいれるデータを作成
+    string_date = "2019/12/" + str(DAY)
+    
+    #時間の取得
+    for TIME in range(0, 24) :
+        start_time = str(TIME) + ":00"
+        end_time = str(TIME) + ":59"
+        #ここでAPIを実行する
+        data_sec = authd_client.intraday_time_series('activities/steps', base_date=DATE, detail_level='1min', start_time = start_time, end_time = end_time )
+        #必要なデータ　をjsonから取得する
+        hour_steps = data_sec['activities-steps'][0]['value']
+        
+        #timeカラム に入れるデータを作成
+        record_time = DATE + " " +str(TIME) + ":00"
+        record_time_with_timezone = record_time + ":00+09"
+        #sql実行
+        cur.execute("INSERT INTO fitbit_hoursteps(hour_steps, time, string_date) VALUES(%s,%s,%s)", (hour_steps, record_time_with_timezone, string_date))
+        print("Now completed insert data time is " + str(TIME))
+        TIME +=1
+        #sqlにコミット 
+    conn.commit()
+    print(DATE + "committed")
